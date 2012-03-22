d2d02bd996be6f14d94cbaf3412d4b3ab086eecc
Jeremy Keczan <keczan@gmail.com>
2012-03-22 16:08:40 -0400

    Updates:
    1. fixed starting issue

--- main.js
+++ main.js
@@ -1,45 +1,41 @@
-/**
- * @author Jeremy Keczan
- */
-
 function animateNext () {
-        $("#images img.current").hide('slow',function(e) {
-          next.addClass("current").show('slow');
-        } );
-        var current = $("#images img.current");
-        var next = current.next();
-        // alert(next.id);
-        if (next.length === 0)
+              $("#images img.current").hide('slow',function(e) {
+                next.addClass("current").show('slow');
+              } );
+              var current = $("#images img.current");
+              var next = current.next();
+              // alert(next.id);
+              if (next.length === 0)
+              {
+                next = $("#images img:first");
+              }
+              next.addClass("current");
+              current.removeClass("current");  
+          }
+      
+      jQuery.fn.AMIinit = function() {
+        
+        var prev = $("#controls").append("<input id=\"prev\" name=\"prev\" type=\"button\" value=\"<<\" /> ");
+        var next = $("#controls").append("<input id=\"next\" name=\"next\" type=\"button\" value=\">>\" /> ");
+        
+        if ($("#images").children("img").length === 0)
         {
-          next = $("#images img:first");
+          alert('no images')
+        } 
+        else
+        {
+          $("#images img").hide();
+          
+          $("#images img").removeAttr("class");
+          $("#images img").addClass("AMIImg");
+          
+          $("#images img:first").addClass("current").show();
+          
+          window.setInterval(animateNext, 7000)
+          //$("#images a img").show();
         }
-        next.addClass("current");
-        current.removeClass("current");
-    }
+      }
       
-jQuery.fn.AMIinit = function() {
-  
-  var prev = $("#controls").append("<input id=\"prev\" name=\"prev\" type=\"button\" value=\"<<\" /> ");
-  var next = $("#controls").append("<input id=\"next\" name=\"next\" type=\"button\" value=\">>\" onclick=\"animateNext('next')\" /> ");
-  
-  if ($("#images").children("img").length === 0)
-  {
-    alert('no images');
-  } 
-  else
-  {
-    $("#images img").hide();
-    
-    $("#images img").removeAttr("class");
-    $("#images img").addClass("AMIImg");
-    
-    $("#images img:first").addClass("current").show();
-    
-    window.setInterval(animateNext(), 7000)
-    //$("#images a img").show();
-  }
-}
-      
-jQuery.fn.AMIImageViewer = function () {
-  $(document).delay(5000).AMIinit();
-}
+      jQuery.fn.AMIImageViewer = function () {
+        $(document).AMIinit();
+      }
